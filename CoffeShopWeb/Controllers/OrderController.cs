ad8dfea0853175a73325ec57424fbaf991e45d96
Linhnguyen202 <ngglinh0914@gmail.com>
2021-10-04 22:58:23 +0700

    AddCategory,orderPage

---
+++ CoffeShopWeb/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeShopWeb.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        public ActionResult Paid()
+        {
+            return View();
+        }
+    }
+}
